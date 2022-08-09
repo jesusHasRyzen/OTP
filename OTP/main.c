9957ca7a75d7898699dc5baa43dfeda90dbbf6b6
Jesus Ponce <jesus.tony.ponce@gmail.com>
2022-08-08 19:49:05 -0700

    Initial Commit

---
+++ OTP/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  OTP
+//
+//  Created by Jesus Ponce on 8/8/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
